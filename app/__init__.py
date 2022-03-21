815e5188d9c7635772d9aac51580314b1aa5e7f3
DevIvo81 <izpy81@gmail.com>
2022-03-21 14:38:49 +0100

    08 - create, update, delete posts

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '02b44f2abc7342ab8286eb3e2a70d995'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+moment = Moment(app)
 
 db = SQLAlchemy(app)
 
